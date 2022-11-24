9dd5b538d3aeaa4be33feba484f20401427c9139
apocalypsemyth <accelerator.apocalypse@gmail.com>
2022-11-24 12:58:03 +0800

    利用ajax實作更新和下拉選單的邏輯

--- js/functions/toDoListFunctions.js
+++ js/functions/toDoListFunctions.js
@@ -32,7 +32,9 @@ function getToDoList(strName) {
               }" class="btn btn-primary btn-sm ms-auto">
                 Update
               </button>
-              <button id="btnDelete_${toDo.id}" class="btn btn-danger btn-sm">
+              <button id="btnDelete_${
+                toDo.id
+              }" class="btn btn-danger btn-sm ms-1">
                 Delete
               </button>
             </li>
@@ -83,7 +85,114 @@ function createToDo(strName) {
               }" class="btn btn-primary btn-sm ms-auto">
                 Update
               </button>
-              <button id="btnDelete_${toDo.id}" class="btn btn-danger btn-sm">
+              <button id="btnDelete_${
+                toDo.id
+              }" class="btn btn-danger btn-sm ms-1">
+                Delete
+              </button>
+            </li>
+          `)
+        }
+      }
+    },
+    error: function (e) {
+      console.log(e)
+      alert('Failed')
+    },
+  })
+}
+
+function updateToDo(strId, strName) {
+  let objPostData = { id: strId, name: strName }
+
+  $.ajax({
+    url: 'http://localhost:8889/updateToDo',
+    method: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify(objPostData),
+    success: function (toDoRes) {
+      let { statusCode, message, toDoList } = toDoRes
+      $('#ulToDoList').empty()
+
+      if (statusCode === '400' || message === '必要參數缺失，請再嘗試。') {
+        $('#ulToDoList').append('必要參數缺失，請再嘗試。')
+      } else if (statusCode === '500' || message === '發生錯誤，請再嘗試。') {
+        $('#ulToDoList').append('發生錯誤，請再嘗試。')
+      } else if (!toDoList || !toDoList.length) {
+        $('#ulToDoList').append('暫無資料。')
+      } else {
+        for (let toDo of toDoList) {
+          $('#ulToDoList').append(`
+            <li class="list-group-item d-flex align-items-center gap-1">
+              <input class="form-check-input me-1 ckb_${
+                toDo.id
+              }" type="checkbox" id="ckb_${toDo.id}" ${
+            toDo.isChecked ? 'checked' : ''
+          }/>
+              <label class="form-check-label align-middle" for="ckb_${
+                toDo.id
+              }">${toDo.name}</label>
+              <button id="btnUpdate_${
+                toDo.id
+              }" class="btn btn-primary btn-sm ms-auto">
+                Update
+              </button>
+              <button id="btnDelete_${
+                toDo.id
+              }" class="btn btn-danger btn-sm ms-1">
+                Delete
+              </button>
+            </li>
+          `)
+        }
+      }
+    },
+    error: function (e) {
+      console.log(e)
+      alert('Failed')
+    },
+  })
+}
+function deleteToDo(strId) {
+  let objPostData = { id: strId }
+
+  $.ajax({
+    url: 'http://localhost:8889/deleteToDo',
+    method: 'POST',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: JSON.stringify(objPostData),
+    success: function (toDoRes) {
+      let { statusCode, message, toDoList } = toDoRes
+      $('#ulToDoList').empty()
+
+      if (statusCode === '400' || message === '必要參數缺失，請再嘗試。') {
+        $('#ulToDoList').append('必要參數缺失，請再嘗試。')
+      } else if (statusCode === '500' || message === '發生錯誤，請再嘗試。') {
+        $('#ulToDoList').append('發生錯誤，請再嘗試。')
+      } else if (!toDoList || !toDoList.length) {
+        $('#ulToDoList').append('暫無資料。')
+      } else {
+        for (let toDo of toDoList) {
+          $('#ulToDoList').append(`
+            <li class="list-group-item d-flex align-items-center gap-1">
+              <input class="form-check-input me-1 ckb_${
+                toDo.id
+              }" type="checkbox" id="ckb_${toDo.id}" ${
+            toDo.isChecked ? 'checked' : ''
+          }/>
+              <label class="form-check-label align-middle" for="ckb_${
+                toDo.id
+              }">${toDo.name}</label>
+              <button id="btnUpdate_${
+                toDo.id
+              }" class="btn btn-primary btn-sm ms-auto">
+                Update
+              </button>
+              <button id="btnDelete_${
+                toDo.id
+              }" class="btn btn-danger btn-sm ms-1">
                 Delete
               </button>
             </li>
--- js/toDoList.js
+++ js/toDoList.js
@@ -5,6 +5,23 @@ $(document).ready(function () {
     e.preventDefault()
 
     let strName = $('#txtName').val()
+    let strId = $(this).attr('href')
+    if (strId) {
+      if (!strName) {
+        alert('請輸入ToDo的名字。')
+        return
+      }
+
+      updateToDo(strId, strName)
+      $(this)
+        .removeAttr('href')
+        .removeClass('btn-outline-primary')
+        .addClass('btn-outline-success')
+        .text('Create')
+      $('#txtName').val('')
+      return
+    }
+
     if (!strName) {
       alert('請輸入ToDo的名字。')
       return
@@ -12,5 +29,46 @@ $(document).ready(function () {
 
     createToDo(strName)
     $('#txtName').val('')
+  })
+
+  $(document).on('click', 'button[id*=btnDelete]', function (e) {
+    e.preventDefault()
+
+    let strId = $(this).prop('id').split('_')[1]
+    deleteToDo(strId)
+  })
+
+  $(document).on('click', 'button[id*=btnUpdate]', function (e) {
+    e.preventDefault()
+
+    $('button[id*=btnUpdate]').prop('disabled', false)
+    $(this).prop('disabled', true)
+
+    $('#txtName').val($(this).siblings('label').text().trim())
+
+    let strId = $(this).prop('id').split('_')[1]
+    $('#btnInInputBar')
+      .attr('href', strId)
+      .removeClass('btn-outline-success')
+      .addClass('btn-outline-primary')
+      .text('Update')
+  })
+
+  $('#ulALinkList .dropdown-item').click(function (e) {
+    e.preventDefault()
+
+    $('#btnInInputBar').removeAttr('href').removeClass('btn-outline-primary')
+
+    if ($(this).hasClass('aLink-create')) {
+      $('#btnInInputBar')
+        .removeClass('btn-outline-secondary')
+        .addClass('btn-outline-success')
+        .text('Create')
+      return
+    }
+    $('#btnInInputBar')
+      .removeClass('btn-outline-success')
+      .addClass('btn-outline-secondary')
+      .text('Search')
   })
 })
